2fefa882f7435f7e559a43e7e4668118fd1a1afa
dhruvmathur2001@gmail.com <dhruvmathur2001@gmail.com>
2025-01-05 21:51:29 +0530

    minor commit

--- Client/src/components/admin-view/Header.jsx
+++ Client/src/components/admin-view/Header.jsx
@@ -12,6 +12,7 @@ const AdminHeader = () => {
       <div className="flex flex-1 justify-end">
         <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
           <LogOut/>
+          Logout
         </Button>
       </div>
        
